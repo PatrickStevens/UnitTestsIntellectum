f903207c0be454815938311f0765c5da2123ced0
patr5651 <pstevens@esri.com>
2014-04-28 15:00:24 -0700

    First commit

---
+++ BasicUnitTests/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicUnitTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestPrivateInvoke()
+    {
+      var a = new DemoLib.A();
+      
+      var methodParams = new object[] { "some stuff" };
+      System.Reflection.MethodInfo reflectedMethod = 
+        a.GetType().GetMethod("PrivateTestStuff", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+      object actualResultObject = reflectedMethod.Invoke(a, methodParams);
+      string actualResultString = actualResultObject.ToString();
+      string expectedResultString = "Hello, World! you can stop doing some stuff now!";
+      Assert.AreEqual(actualResultString, expectedResultString);
+    }
+
+    private void TestGenericInvoke<T>()
+    {
+      var aofT = new DemoLib.A<T>();
+
+      object[] methodParams = null;
+
+      var genericType1 = typeof(DemoLib.A<>).MakeGenericType(typeof(T));
+      var genericType2 = aofT.GetType();
+      Assert.AreEqual(genericType1.ToString(), genericType2.ToString());
+
+      var reflectedMethod = genericType1.GetMethod("GetSomeGenericStuff");
+
+      object actualResultObject = reflectedMethod.Invoke(aofT, methodParams);
+      string actualResultString = actualResultObject.ToString();
+      string expectedResultString = String.Format("System.Collections.Generic.List`1[{0}]", typeof(T).ToString());
+      Assert.AreEqual(actualResultString, expectedResultString);
+    }
+
+    [TestMethod]
+    public void TestGenericIntInvoke()
+    {
+      TestGenericInvoke<int>();
+    }
+
+    [TestMethod]
+    public void TestGenericDateTimeInvoke()
+    {
+      TestGenericInvoke<DateTime>();
+    }
+
+  }
+}
---
+++ DemoLib/A.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib
+{
+    public class A
+    {
+      private string PrivateTestStuff(string stuff = "PrivateTestStuff")
+      {
+        System.Diagnostics.Debug.WriteLine(String.Format("Called Private Test and did {0}", stuff));
+        return String.Format("Hello, World! you can stop doing {0} now!", stuff);
+      }
+    }
+
+    public class A<T>
+    {
+      public List<T> GetSomeGenericStuff()
+      {
+        var value = default(T);
+        var listStuff = new List<T>();
+        listStuff.Add(value);
+
+        return listStuff;
+      }
+    }   
+}
